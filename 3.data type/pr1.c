5d3d0567a3574b8c0d1257de41c3704ea1a4bd68
chavan krishna <68773719+chavank901@users.noreply.github.com>
2023-08-05 18:35:55 +0000

    second

---
+++ 3.data type/pr1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    int b=-20;
+    int c=30U;
+    int d=40L;
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    printf("c=%d\n",c);
+    printf("d=%d\n",d);
+}
